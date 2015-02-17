287d4cac35bbcace620d14eebd91dec9701cd4f4
Waqas Ahmed <wakqasahmed@gmail.com>
2015-02-17 15:34:13 +0400

    config changed

--- server.js
+++ server.js
@@ -4,6 +4,9 @@
 // Set the 'NODE_ENV' variable
 process.env.NODE_ENV = process.env.NODE_ENV || 'production';
 
+var ip_addr = process.env.OPENSHIFT_NODEJS_IP   || '127.0.0.1';
+var port    = process.env.OPENSHIFT_NODEJS_PORT || '3000';
+
 // Load the module dependencies
 var mongoose = require('./config/mongoose'),
 	express = require('./config/express'),
@@ -13,16 +16,24 @@ var mongoose = require('./config/mongoose'),
 var db = mongoose();
 
 // Create a new Express application instance
-var app = express();
+//var app = express();
+var app = express({
+		name : process.env.OPENSHIFT_APP_NAME || "Surveymark"
+});
+
 
 // Configure the Passport middleware
 var passport = passport();
 
 // Use the Express application instance to listen to the '3000' port
-app.listen(3000);
+//app.listen(3000);
+
+app.listen(port ,ip_addr, function(){
+    console.log('Server running at %s ', 'http://' + ip_addr + ':' + port);
+});
 
 // Log the server status to the console
-console.log('Server running at http://localhost:3000/');
+//console.log('Server running at http://localhost:3000/');
 
 // Use the module.exports property to expose our Express application instance for external usage
 module.exports = app;
