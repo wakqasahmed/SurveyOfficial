898f16d2e3e8237a83e38db5d429b5b8d4dca852
Waqas Ahmed <wakqasahmed@gmail.com>
2015-04-27 14:20:11 +0400

    Chakir date picker added
    Chakir date picker added

--- public/reports/controllers/reports.client.controller.js
+++ public/reports/controllers/reports.client.controller.js
@@ -81,6 +81,55 @@ angular.module('reports').controller('reportsController', ['$scope', '$routePara
 
 
         ////////////////////////////////////////
+
+
+
+        $('#reportrange span').html(moment().subtract(29, 'days').format('MMMM D, YYYY') + ' - ' + moment().format('MMMM D, YYYY'));
+
+        $('#reportrange').daterangepicker({
+            format: 'MM/DD/YYYY',
+            startDate: moment().subtract(29, 'days'),
+            endDate: moment(),
+            minDate: '01/01/2012',
+            maxDate: '12/31/2015',
+            dateLimit: { days: 60 },
+            showDropdowns: true,
+            showWeekNumbers: true,
+            timePicker: false,
+            timePickerIncrement: 1,
+            timePicker12Hour: true,
+            ranges: {
+                'Today': [moment(), moment()],
+                'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+                'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+                'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+                'This Month': [moment().startOf('month'), moment().endOf('month')],
+                'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+            },
+            opens: 'left',
+            drops: 'down',
+            buttonClasses: ['btn', 'btn-sm'],
+            applyClass: 'btn-primary',
+            cancelClass: 'btn-default',
+            separator: ' to ',
+            locale: {
+                applyLabel: 'Submit',
+                cancelLabel: 'Cancel',
+                fromLabel: 'From',
+                toLabel: 'To',
+                customRangeLabel: 'Custom',
+                daysOfWeek: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr','Sa'],
+                monthNames: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+                firstDay: 1
+            }
+        }, function(start, end, label) {
+            console.log(start.toISOString(), end.toISOString(), label);
+
+            startD = start.toISOString() ;
+            endD = end.toISOString() ;
+
+            $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+        });
         /////////// ///          /// //////////
 
 
@@ -448,6 +497,8 @@ angular.module('reports').controller('reportsController', ['$scope', '$routePara
             // Use the form fields to create a new article $resource object
             var data ={
                 //TotalCheck: this.checks
+                'startDate': startD,//field.field_brandID,
+                'endDate': endD,
                 'brandId': $scope.selectedBrandIds,//field.field_brandID,
               'questionId': field.field_selectedQuestionID
                                   //,shifts:[{value:"breakfast",from:5,to:12},{value:"lunch",from:12,to:18},{value:"dinner",from:18,to:23},{value:"brunch",from:0,to:4}] ,
@@ -678,6 +729,8 @@ angular.module('reports').controller('reportsController', ['$scope', '$routePara
             // Use the form fields to create a new article $resource object
             var data ={
                 //TotalCheck: this.checks
+                'startDate': startD,//field.field_brandID,
+                'endDate': endD,
                 'brandId': $scope.selectedBrandIds,//field.field_brandID,
                 'questionId': field.field_selectedQuestionID
                 //,shifts:[{value:"breakfast",from:5,to:12},{value:"lunch",from:12,to:18},{value:"dinner",from:18,to:23},{value:"brunch",from:0,to:4}] ,
@@ -1125,7 +1178,10 @@ angular.module('reports').controller('reportsController', ['$scope', '$routePara
 
                         $scope.locationNames.push(data[k].name);
                         $scope.locationiD.push(data[k]._id);
-                        $scope.locationColors.push(data[k].brand.color)
+
+                        $scope.locationColors.push(data[k].brand.color);
+
+
 
                     }
 
@@ -1158,8 +1214,8 @@ angular.module('reports').controller('reportsController', ['$scope', '$routePara
 
             var data ={
                 //TotalCheck: this.checks
-                'startDate': start.value(),//field.field_brandID,
-                'endDate': end.value()
+                'startDate': startD,//field.field_brandID,
+                'endDate': endD
                 //,shifts:[{value:"breakfast",from:5,to:12},{value:"lunch",from:12,to:18},{value:"dinner",from:18,to:23},{value:"brunch",from:0,to:4}] ,
 
                 //days:["Thu","Wed","Mon","Sun","Fri","Sat"]
@@ -1234,7 +1290,7 @@ angular.module('reports').controller('reportsController', ['$scope', '$routePara
                                 visible: true
                             },
                             title: {
-                                text: "Survey presented"
+                                text: "Guest checked"
                             }
                         },
                         majorGridLines: {
@@ -1285,8 +1341,14 @@ angular.module('reports').controller('reportsController', ['$scope', '$routePara
         }
 
         $scope.loadGuestSurveys = function (){
+            var data ={
 
-            $http.get('/api/reports/monthly/participationRate').
+                'startDate': startD,//field.field_brandID,
+                'endDate': endD
+
+            };
+
+            $http.get('/api/reports/monthly/participationRate',data).
                 success(function(docs, status, headers, config) {
                     // this callback will be called asynchronously
                     // when the response is available
@@ -1350,7 +1412,7 @@ angular.module('reports').controller('reportsController', ['$scope', '$routePara
                                 visible: true
                             },
                             title: {
-                                text: "Total checks"
+                                text: "Guest Surveys"
                             }
                         },
                         majorGridLines: {
